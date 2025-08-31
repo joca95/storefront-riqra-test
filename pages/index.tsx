be58ce6505abeb1d4d0c72543a771e5c4df9b2d0
Kenyi Mendoza <46731141+joca95@users.noreply.github.com>
2025-08-30 23:14:50 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,8 @@ export default function HomePage({ text }) {
 }
 
 export async function getServerSideProps() {
-  const response = await fetch(`http://${process.env.API}`)
+  //const response = await fetch(`http://${process.env.API}`)
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/ditto`)
 
   const body = await response.text()
 
